7187f4781da0bab1ce8eff1931ef668dde997f6b
DiegoV22 <diego.vega.aguilera@udla.edu.ec>
2025-07-09 13:56:03 -0500

    Fix export getUsuarios + build ready for deploy

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://backend-health-06rn.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://backend-health-06rn.onrender.com/api/",
 });
 
 export default api;
